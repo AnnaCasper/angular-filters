9ea5a5fab46c033610d7616df6af8d3263541eac
Anna Casper <anna.casper3@gmail.com>
2015-09-09 07:39:46 -0600

    itunes http get request complete

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,9 @@
-app.controller('MainCtrl', function ($scope) {
+app.controller('MainCtrl', function ($scope, $http) {
   $scope.result = 'test';
+  $http.get('https://api.github.com/zen').then(function(data){
+    $scope.zenData = data;
+  });
+  $http.get('itunes.json').then(function (data) {
+    $scope.appleData = data;
+  });
 })
